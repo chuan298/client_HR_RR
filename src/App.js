010fdf4a0377fe87d9c38387262f79afdae3d9a6
Ancv <chuvanan29081998@gmail.com>
2021-11-11 01:33:55 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  state = {
+
+    // Initially, no file is selected
+    selectedFile: null,
+    HR: "",
+    RR: ""
+  };
+
+  // On file select (from the pop up)
+  onFileChange = (event) => {
+
+    // Update the state
+    this.setState({ selectedFile: event.target.files[0] });
+
+  };
+
+  // On file upload (click the upload button)
+  onFileUpload = (file) => {
+
+
+    const formData = new FormData();
+    formData.append('file',file)
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    return axios.post(process.env.REACT_APP_API_ENDPOINT, formData,config)
+  };
+  onFormSubmit = (e) => {
+    e.preventDefault() // Stop form submit
+    this.setState({HR:"", RR: ""})
+    this.onFileUpload(this.state.selectedFile).then((response)=>{
+      console.log(response.data);
+      this.setState({HR:response.data.HR, RR: response.data.RR})
+    })
+  }
+  // File content to be displayed after
+  // file upload is complete
+  
+  render() {
+    return (
+      <div>
+        <h1>
+          Demo Heart rate and Respiration rate
+            </h1>
+
+        {/* <div>
+          <input type="file" onChange={this.onFileChange} />
+          <button onClick={this.onFileUpload}>
+            Upload!
+                </button>
+        </div> */}
+        <form onSubmit={this.onFormSubmit}>
+          <h1> </h1>
+          <input type="file" onChange={this.onFileChange} />
+          <button type="submit">Upload</button>
+        </form>
+        {/* {this.fileData()} */}
+        <div>
+          <h2>Heart Rate: {this.state.HR}</h2>
+          <h2>Respiration Rate: {this.state.RR}</h2>
+
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
